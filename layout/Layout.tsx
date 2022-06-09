31123722c9078193eeea85ea354ea6ed1c2033f3
paha <pahany4@mail.ru>
2022-06-09 15:19:29 +0700

    верстка layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { LayoutProps } from './Layout.props';
+import styles from './Layout.module.css';
 import { Header } from './Header/Header';
 import React, { FunctionComponent } from 'react';
 import { Sidebar } from './Sidebar/Sidebar';
@@ -6,16 +7,14 @@ import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>
-					{children}
-				</div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
+				{children}
 			</div>
-			<Footer />
-		</>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
