bf821ae1d03b338e6d5376a854dc4eae70d8f549
paha <pahany4@mail.ru>
2022-06-10 16:55:29 +0700

    getStaticProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
 import Head from 'next/head';
 import React from 'react';
-import {AppProps} from "next/dist/shared/lib/router/router";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return <>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,24 @@
-import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps} from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx);
-        return {...initialProps};
-    }
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-    render(): JSX.Element {
-        return (
-            <Html lang={"ru"}>
-                <Head>
-                    <title>Nextjs</title>
-                </Head>
-                <body>
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
-        );
-    }
+	render(): JSX.Element {
+		return (
+			<Html lang="ru">
+				<Head>
+					<title>Nextjs</title>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
